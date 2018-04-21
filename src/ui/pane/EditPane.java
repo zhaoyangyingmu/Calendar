6fbaaf8bba4778a6636b0db7fbf7fd235c1f9cd9
zhaoyangyingmu <16302010029@fudan.edu.cn>
2018-04-21 19:46:26 +0800

    EditPane refactored successfully!

--- src/ui/pane/EditPane.java
+++ src/ui/pane/EditPane.java
@@ -27,30 +27,20 @@ public class EditPane extends GridPane {
         Label fromLabel = new Label("From: ");
         fromRow.add(fromLabel , 0 , 0);
 
-        Label fromYear = new Label("Year:");
-        fromRow.add(fromYear , 1 , 0);
-        TextField fromYearText = new TextField(from.getYear()+"");
-        fromRow.add(fromYearText , 2 , 0);
+        GridPane fromYearRow = new LabelAndTextRow("Year: ", from.getYear()+"");
+        fromRow.add(fromYearRow , 1 , 0);
 
-        Label fromMonth = new Label("Month: ");
-        fromRow.add(fromMonth, 3 , 0);
-        TextField fromMonthText = new TextField(from.getMonth()+"");
-        fromRow.add(fromMonthText, 4, 0);
+        GridPane fromMonthRow = new LabelAndTextRow("Month: ", from.getMonth()+"");
+        fromRow.add(fromMonthRow, 2, 0);
 
-        Label fromDay = new Label("Day: ");
-        fromRow.add(fromDay , 5, 0);
-        TextField fromDayText = new TextField(from.getDay()+"");
-        fromRow.add(fromDayText , 6, 0);
+        GridPane fromDayRow = new LabelAndTextRow("Day: ", from.getMonth()+ "");
+        fromRow.add(fromDayRow , 3, 0);
 
-        Label fromHour = new Label("Hour: ");
-        fromRow.add(fromHour , 7 , 0);
-        TextField fromHourText = new TextField(from.getHour() + "");
-        fromRow.add(fromHourText , 8, 0);
+        GridPane fromHourRow = new LabelAndTextRow("Hour: " , from.getDay()+ "");
+        fromRow.add(fromHourRow , 4, 0);
 
-        Label fromMinute = new Label("Minute: ");
-        fromRow.add(fromMinute , 9 , 0);
-        TextField fromMinuteText = new TextField(from.getMinute()+"");
-        fromRow.add(fromMinuteText, 10 , 0);
+        GridPane fromMinuteRow = new LabelAndTextRow("Minute: ", from.getMinute() + "");
+        fromRow.add(fromMinuteRow, 5, 0);
         this.add(fromRow , 0  , 0 );
 
         GridPane toRow = new GridPane();
@@ -58,44 +48,26 @@ public class EditPane extends GridPane {
         Label toLabel = new Label("To: ");
         toRow.add(toLabel , 0 , 0);
 
-        Label toYear = new Label("Year:");
-        toRow.add(toYear , 1 , 0);
-        TextField toYearText = new TextField(to.getYear()+"");
-        toRow.add(toYearText , 2 , 0);
+        GridPane toYearRow = new LabelAndTextRow("Year: ", to.getYear()+"");
+        toRow.add(toYearRow , 2 , 0);
 
-        Label toMonth = new Label("Month: ");
-        toRow.add(toMonth, 3 , 0);
-        TextField toMonthText = new TextField(to.getMonth()+"");
-        toRow.add(toMonthText, 4, 0);
+        GridPane toMonthRow = new LabelAndTextRow("Month: " , to.getMonth()+"");
+        toRow.add(toMonthRow, 4, 0);
 
-        Label toDay = new Label("Day: ");
-        toRow.add(toDay , 5, 0);
-        TextField toDayText = new TextField(to.getDay()+"");
-        toRow.add(toDayText , 6, 0);
+        GridPane toDayRow = new LabelAndTextRow("Day: ", to.getDay()+"");
+        toRow.add(toDayRow , 6, 0);
 
-        Label toHour = new Label("Hour: ");
-        toRow.add(toHour , 7 , 0);
-        TextField toHourText = new TextField(to.getHour() + "");
-        toRow.add(toHourText , 8, 0);
+        GridPane toHourRow = new LabelAndTextRow("Hour: " , to.getHour()+"");
+        toRow.add(toHourRow , 8, 0);
 
-        Label toMinute = new Label("Minute: ");
-        toRow.add(toMinute , 9 , 0);
-        TextField toMinuteText = new TextField(to.getMinute()+"");
-        toRow.add(toMinuteText, 10 , 0);
+        GridPane toMinuteRow = new LabelAndTextRow("Minute: " , to.getMinute()+ "");
+        toRow.add(toMinuteRow, 10 , 0);
         this.add(toRow, 0  , 1 );
 
-        GridPane typeRow = new GridPane();
-        Label typeLabel = new Label("type: ");
-        typeRow.add(typeLabel , 0 , 0);
-        TextField typeText = new TextField(""+ item.getItemType().toString());
-        typeRow.add(typeText , 1, 0);
+        GridPane typeRow = new LabelAndTextRow("type: ", item.getItemType().toString());
         this.add(typeRow, 0 , 2);
 
-        GridPane infoRow = new GridPane();
-        Label infoLabel = new Label("Info: ");
-        infoRow.add(infoLabel , 0 , 0);
-        TextField infoText = new TextField("" + item.getDetailText());
-        infoRow.add(infoText , 1, 0);
+        GridPane infoRow = new LabelAndTextRow("Info: ", item.getDetailText());
         this.add(infoRow , 0, 3);
 
         GridPane buttonRow = new GridPane();
@@ -113,6 +85,24 @@ public class EditPane extends GridPane {
     }
 
 
+    private class LabelAndTextRow extends GridPane{
+        private Label label;
+        private TextField textField;
+        public LabelAndTextRow(String labelStr, String textStr) {
+            label = new Label(labelStr);
+            textField = new TextField(textStr);
+            this.add(label , 0 , 0);
+            this.add(textField , 1, 0);
+        }
+
+        public Label getLabel() {
+            return label;
+        }
+
+        public TextField getTextField() {
+            return textField;
+        }
+    }
 //  public void setContent(Item item)
 // TODO: 2018/4/21
 
