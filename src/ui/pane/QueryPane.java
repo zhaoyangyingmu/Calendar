c03b97ca521be59a879baf73b5d778717644a3bb
Oops <16302010059@fudan.edu.cn>
2018-05-08 22:12:04 +0800

    优化单例模式的实现，降低线程不安全性

--- src/ui/pane/QueryPane.java
+++ src/ui/pane/QueryPane.java
@@ -17,14 +17,17 @@ import ui.Config;
  * Created by 谢东方xdf on 2018/3/25.
  */
 public class QueryPane extends StackPane {
-    private static QueryPane queryPane;
-    private GridPane contentGrid;
+    private static volatile QueryPane queryPane;
 
     public static QueryPane getInstance() {
         if (queryPane == null) {
-            queryPane = new QueryPane();
-            queryPane.getStylesheets().add(Config.class.getResource("/stylesheet/query.css").toString());
-            queryPane.getStylesheets().add(Config.class.getResource("/stylesheet/buttonAndLabel.css").toString());
+            synchronized (QueryPane.class) {
+                if (queryPane == null) {
+                    queryPane = new QueryPane();
+                    queryPane.getStylesheets().add(Config.class.getResource("/stylesheet/query.css").toString());
+                    queryPane.getStylesheets().add(Config.class.getResource("/stylesheet/buttonAndLabel.css").toString());
+                }
+            }
         }
         return queryPane;
     }
@@ -33,36 +36,36 @@ public class QueryPane extends StackPane {
         Label hintLabel = new Label("HintLabel");
         hintLabel.setStyle("-fx-text-fill: white ; -fx-font-size: 20");
         TextField dateText = new TextField("Search format : 2018-1-1");
-        dateText.setMaxSize(200 , 30);
-        dateText.setMinSize(200 , 30);
+        dateText.setMaxSize(200, 30);
+        dateText.setMinSize(200, 30);
         Button searchBt = new Button("Search");
         searchBt.getStyleClass().add("btn");
-        searchBt.setMaxSize(80,30);
-        searchBt.setMinSize(80,30);
+        searchBt.setMaxSize(80, 30);
+        searchBt.setMinSize(80, 30);
         searchBt.setCursor(Cursor.HAND);
         searchBt.setOnMouseClicked(event -> {
             String text = dateText.getText();
             try {
                 CalendarDate date = new CalendarDate(text);
-                if (!Display.paintDays(date)){
+                if (!Display.paintDays(date)) {
                     hintLabel.setText("InvalidDate");
                     hintLabel.setStyle("-fx-text-fill: red;; -fx-font-size: 15;");
-                }else {
+                } else {
                     hintLabel.setText("HintLabel");
                     hintLabel.setStyle("-fx-text-fill: white ; -fx-font-size: 20");
                 }
-            }catch (InvalidDateException e){
+            } catch (InvalidDateException e) {
                 hintLabel.setText("InvalidFormat");
                 hintLabel.setStyle("-fx-text-fill: red;; -fx-font-size: 15;");
             }
         });
-        contentGrid = new GridPane();
-        contentGrid.add(hintLabel , 0 , 0);
+        GridPane contentGrid = new GridPane();
+        contentGrid.add(hintLabel, 0, 0);
         contentGrid.add(dateText, 1, 0);
         contentGrid.add(searchBt, 2, 0);
         contentGrid.setHgap(15);
         contentGrid.setAlignment(Pos.CENTER);
-        contentGrid.setPadding(new Insets(10,0,0,0));
+        contentGrid.setPadding(new Insets(10, 0, 0, 0));
         this.getChildren().add(contentGrid);
     }
 
