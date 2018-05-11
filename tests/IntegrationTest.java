20d9464bbfbf8915223896892b8a7e70264ac7ec
Oops <16302010059@fudan.edu.cn>
2018-05-11 19:36:52 +0800

    添加集成测试

---
+++ tests/IntegrationTest.java
@@ -0,0 +1,171 @@
+import exception.InvalidDateException;
+import io.ItemIO;
+import kernel.CalendarDate;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import todoitem.Item;
+import todoitem.ItemManager;
+import todoitem.itemSub.Appointment;
+import todoitem.itemSub.Meeting;
+import todoitem.itemSub.OtherItem;
+import todoitem.util.TimeStamp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+/**
+ * # 集成测试，围绕待办事项测试
+ * 一、信息输入（ --> 待办事项）时间字符串 =》 CalendarDate对象 =》 TimeStamp对象 =》 待办事项实例 =》 检查时间
+ * 1.时间选择
+ * - 起始时间    （字符串）
+ * - 截止时间    （字符串）
+ * 2.类型选择
+ * - 会议
+ * - 约会
+ * - 其他
+ * 3.详细信息
+ * 4，得到待办事项对象，检查待办事项属性（Test)
+ * 5.存入
+ * 二、数据库（ --> 待办事项）数据库 =》 Item(待办事项)对象 =》 检查待办事项 =》 TimeStamp => CalendarDate对象 =》 检查时间
+ * 1.读取
+ * 2.读取时间
+ * - 检查起始时间   (Test)
+ * - 检查截止时间   (Test)
+ * 3.读取类型
+ * - 检查待办事项类型 (Test)
+ * 4.读取详细信息
+ */
+@RunWith(Parameterized.class)
+public class IntegrationTest {
+    private final static ArrayList<Item> expectedItems = new ArrayList<>();
+
+    private String frDateString;
+    private String toDateString;
+    private boolean frDateIsNull;
+    private boolean toDateIsNull;
+    private boolean isDuringTime;
+    private Item.ItemType type;
+
+    public IntegrationTest(String frDateString, String toDateString,
+                           boolean frDateIsNull, boolean toDateIsNull,
+                           boolean isDuringTime, Item.ItemType type) {
+        this.frDateString = frDateString;
+        this.toDateString = toDateString;
+        this.frDateIsNull = frDateIsNull;
+        this.toDateIsNull = toDateIsNull;
+        this.isDuringTime = isDuringTime;
+        this.type = type;
+    }
+
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getTestData() {
+        return Arrays.asList(new Object[][]{
+                {"2018-5-1", "2018-4-29", false, false, false, Item.ItemType.LEISURE},     //非法时间段，不能创建待办事项对象
+                {"2018-1-1", "2017-12-30", false, false, false, Item.ItemType.LEISURE},    //非法时间段
+                {"2016-2-30", "2016-3-1", true, false, false, Item.ItemType.LEISURE},      //非法日期
+                {"2000-2-29", "2018-2-29", false, true, false, Item.ItemType.LEISURE},     //非法日期
+                {"2020-2-29", "2020-3-0", false, true, false, Item.ItemType.LEISURE},      //非法日期
+                {"0000-00-00", "2018-5-12", true, false, false, Item.ItemType.LEISURE},    //非法日期
+                {"2018-2-29", "2018-4-29", false, false, false, Item.ItemType.LEISURE},    //非法日期
+                {"2018-2-28", "2018-3-1", false, false, true, Item.ItemType.LEISURE},
+                {"2000-2-29", "2018-2-29", false, false, true, Item.ItemType.STUDY},
+                {"2020-2-29", "2018-4-1", false, false, true, Item.ItemType.DATING},
+                {"2018-5-10", "2018-5-12", false, false, true, Item.ItemType.LEISURE},
+                {"2018-5-9", "2018-5-11", false, false, true, Item.ItemType.STUDY},
+                {"2018-5-10", "2018-5-12", false, false, true, Item.ItemType.DATING},
+        });
+    }
+
+    @Test
+    public void ItemTest() {
+        CalendarDate frDate = null;
+        CalendarDate toDate = null;
+        TimeStamp frTime = null;
+        TimeStamp toTime = null;
+        try {
+            frDate = new CalendarDate(frDateString);
+            frTime = new TimeStamp(frDate.getYear(), frDate.getMonth(), frDate.getDay(), 0, 0);
+        } catch (InvalidDateException e) {
+            assertTrue(frDateIsNull);
+            assertNull(frDate);
+        }
+        try {
+            toDate = new CalendarDate(toDateString);
+            toTime = new TimeStamp(toDate.getYear(), toDate.getMonth(), toDate.getDay(), 23, 59);
+        } catch (InvalidDateException e) {
+            assertTrue(toDateIsNull);
+            assertNull(toDate);
+        }
+        Item item;
+        if (type == Item.ItemType.LEISURE) {
+            item = new OtherItem(frTime, toTime, "", type);
+        } else if (type == Item.ItemType.DATING)
+            item = new Appointment(frTime, toTime, "", type, "", "");
+        else
+            item = new Meeting(frTime, toTime, "", type, "", "");
+
+        if (isDuringTime) {
+            TimeStamp actualFrTime = item.getFrom();
+            TimeStamp actualToTime = item.getTo();
+            CalendarDate actualFrDate = new CalendarDate(actualFrTime.getYear(), actualFrTime.getMonth(), actualFrTime.getDay());
+            CalendarDate actualToDate = new CalendarDate(actualToTime.getYear(), actualToTime.getMonth(), actualToTime.getDay());
+            assertEquals(frTime, actualFrTime);
+            assertEquals(toTime, actualToTime);
+            assertEquals(type, item.getItemType());
+            assertEquals("", item.getDetailText());
+            assertEquals(frDate, actualFrDate);
+            assertEquals(toDate, actualToDate);
+
+            expectedItems.add(item);
+            //存入数据库
+            ItemManager manager = ItemManager.getInstance();
+            manager.addItem(item);
+            ItemIO.output();
+        } else {
+            assertNull(item);
+        }
+
+    }
+
+    @Test
+    public void IOTest() {
+        ItemIO.input();
+        ItemManager manager = ItemManager.getInstance();
+        ArrayList<Item> items = manager.getItemList();
+        assertEquals(expectedItems.size(), items.size());
+
+        for (int i = 0; i < expectedItems.size(); i++) {
+            assertEquals(expectedItems.get(i), items.get(i));
+            assertEquals(expectedItems.get(i).getFrom(), items.get(i).getFrom());
+            assertEquals(expectedItems.get(i).getTo(), items.get(i).getTo());
+            assertEquals(expectedItems.get(i).getItemType(), items.get(i).getItemType());
+            assertEquals(expectedItems.get(i).getDetailText(), items.get(i).getDetailText());
+        }
+        for (int i = 0; i < 5; i++) {
+            Item item = new OtherItem(new TimeStamp(2018, 5, i + 1, 0, 0),
+                    new TimeStamp(2018, 5, i + 1, 23, 59),
+                    "", Item.ItemType.LEISURE);
+            expectedItems.add(item);
+            manager.addItem(item);
+            ItemIO.output();
+            ItemIO.input();
+            assertEquals(expectedItems.size(), manager.getItemList().size());
+        }
+        int size = expectedItems.size();
+        for (Item item : expectedItems) {
+            manager.deleteItem(item);
+            size--;
+            ItemIO.output();
+            ItemIO.input();
+            assertEquals(size, manager.getItemList().size());
+        }
+
+    }
+}
--- tests/todoitem/ItemTest.java
+++ tests/todoitem/ItemTest.java
@@ -26,75 +26,84 @@ public class ItemTest {
     }
 
     @Test
+    public void NullItemInstance() {
+        TimeStamp from = new TimeStamp(2018, 5, 11, 0, 0);
+        TimeStamp to = new TimeStamp(2018, 5, 10, 0, 0); //非法时间段，不应该创建待办事项实例
+        Item item = new OtherItem(from, to, "", Item.ItemType.LEISURE);
+        assertNull(item);
+    }
+
+    @Test
     public void isDuringTime() {
-        List<TimeStamp> fromCases = new ArrayList<TimeStamp>(){
+        List<TimeStamp> fromCases = new ArrayList<TimeStamp>() {
             {
-                add(new TimeStamp(2016, 2,29, 0 , 0));
-                add(new TimeStamp(2013, 1,31, 16 , 6));
-                add(new TimeStamp(2018, 6,30, 5 , 42));
+                add(new TimeStamp(2016, 2, 29, 0, 0));
+                add(new TimeStamp(2013, 1, 31, 16, 6));
+                add(new TimeStamp(2018, 6, 30, 5, 42));
             }
         };
-        List<TimeStamp> toCases = new ArrayList<TimeStamp>(){
+        List<TimeStamp> toCases = new ArrayList<TimeStamp>() {
             {
-                add(new TimeStamp(2016, 2,29, 6 , 31));
-                add(new TimeStamp(2014, 1,31, 16 , 6));
-                add(new TimeStamp(2018, 6,30, 5 , 43));
+                add(new TimeStamp(2016, 2, 29, 6, 31));
+                add(new TimeStamp(2014, 1, 31, 16, 6));
+                add(new TimeStamp(2018, 6, 30, 5, 43));
             }
         };
         List<Item> itemsDuringThisPeriod = new ArrayList<Item>() {
             {
-                add(new Appointment(new TimeStamp(2016, 2,29, 0 , 1),
-                             new TimeStamp(2016, 2,29, 0 , 2),
-                        "", Item.ItemType.LEISURE, "" , ""));
+                add(new Appointment(new TimeStamp(2016, 2, 29, 0, 1),
+                        new TimeStamp(2016, 2, 29, 0, 2),
+                        "", Item.ItemType.LEISURE, "", ""));
 
-                add(new Appointment(new TimeStamp(2013, 1,31, 23 , 1),
-                        new TimeStamp(2014, 1,31, 16 , 6),
-                        "", Item.ItemType.LEISURE, "" , ""));
+                add(new Appointment(new TimeStamp(2013, 1, 31, 23, 1),
+                        new TimeStamp(2014, 1, 31, 16, 6),
+                        "", Item.ItemType.LEISURE, "", ""));
 
-                add(new Appointment(new TimeStamp(2018, 6,30, 5 , 42),
-                        new TimeStamp(2018, 6,30, 5 , 43),
-                        "", Item.ItemType.LEISURE, "" , ""));
+                add(new Appointment(new TimeStamp(2018, 6, 30, 5, 42),
+                        new TimeStamp(2018, 6, 30, 5, 43),
+                        "", Item.ItemType.LEISURE, "", ""));
             }
         };
         for (int i = 0; i < fromCases.size(); i++) {
-            assertTrue(itemsDuringThisPeriod.get(i).isDuringTime(fromCases.get(i),toCases.get(i)));
+            assertTrue(itemsDuringThisPeriod.get(i).isDuringTime(fromCases.get(i), toCases.get(i)));
         }
+
     }
 
     @Test
     public void setFrom() {
-        Item item = new Meeting(new TimeStamp(2016, 2,29, 0 , 1),
-                new TimeStamp(2016, 2,29, 0 , 2),
-                "", Item.ItemType.LEISURE, "" , "");
-        TimeStamp timeStamp = new TimeStamp(2018,4,14,20,25);
+        Item item = new Meeting(new TimeStamp(2016, 2, 29, 0, 1),
+                new TimeStamp(2016, 2, 29, 0, 2),
+                "", Item.ItemType.LEISURE, "", "");
+        TimeStamp timeStamp = new TimeStamp(2018, 4, 14, 20, 25);
         item.setFrom(timeStamp);
-        assertEquals(timeStamp,item.getFrom());
+        assertEquals(timeStamp, item.getFrom());
     }
 
     @Test
     public void setTo() {
-        Item item = new OtherItem(new TimeStamp(2016, 2,29, 0 , 1),
-                new TimeStamp(2016, 2,29, 0 , 2), "", Item.ItemType.LEISURE);
-        TimeStamp timeStamp = new TimeStamp(2018,4,14,20,25);
+        Item item = new OtherItem(new TimeStamp(2016, 2, 29, 0, 1),
+                new TimeStamp(2016, 2, 29, 0, 2), "", Item.ItemType.LEISURE);
+        TimeStamp timeStamp = new TimeStamp(2018, 4, 14, 20, 25);
         item.setTo(timeStamp);
-        assertEquals(timeStamp,item.getTo());
+        assertEquals(timeStamp, item.getTo());
     }
 
     @Test
     public void setDetailText() {
-        Item item = new OtherItem(new TimeStamp(2016, 2,29, 0 , 1),
-                new TimeStamp(2016, 2,29, 0 , 2), "", Item.ItemType.LEISURE);
+        Item item = new OtherItem(new TimeStamp(2016, 2, 29, 0, 1),
+                new TimeStamp(2016, 2, 29, 0, 2), "", Item.ItemType.LEISURE);
         String detailText = "HANG ON!";
         item.setDetailText(detailText);
-        assertEquals(detailText,item.getDetailText());
+        assertEquals(detailText, item.getDetailText());
     }
 
     @Test
     public void setItemType() {
-        Item item = new OtherItem(new TimeStamp(2016, 2,29, 0 , 1),
-                new TimeStamp(2016, 2,29, 0 , 2), "", Item.ItemType.LEISURE);
+        Item item = new OtherItem(new TimeStamp(2016, 2, 29, 0, 1),
+                new TimeStamp(2016, 2, 29, 0, 2), "", Item.ItemType.LEISURE);
         Item.ItemType type = Item.ItemType.DATING;
         item.setItemType(Item.ItemType.DATING);
-        assertEquals(type,item.getItemType());
+        assertEquals(type, item.getItemType());
     }
 }
