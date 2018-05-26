1b73651910a911567706399006c241cb6beaadf9
Oops <16302010059@fudan.edu.cn>
2018-05-26 18:57:12 +0800

    修改存储数据类型

--- src/todoitem/AttributeMap.java
+++ src/todoitem/AttributeMap.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public interface AttributeMap {
-    HashMap<String, Object> getAttrs();
+    HashMap<String, String> getAttrs();
 
-    Object getValue(String key);
+    String getValue(String key);
 
-    void addAttr(String key, Object value);
+    void addAttr(String key, String value);
 
     void removeAttr(String key);
 
 
-    void addAttrs(Map<String, Object> attrs);
+    void addAttrs(Map<String, String> attrs);
 }
