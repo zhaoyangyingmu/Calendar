9bdfdc692141052d363f675abbbc3fb5a73a3dc9
Oops <16302010059@fudan.edu.cn>
2018-05-27 17:27:37 +0800

    数据库

---
+++ src/todoitem/Mysql.java
@@ -0,0 +1,32 @@
+package todoitem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Mysql {
+    public static int addSchedule(HashMap<String, String> hashMap) {
+        return 0;
+    }
+
+    public static ArrayList<HashMap<String, String>> queryByTime(String startTime, String endTime) {
+        return null;
+    }
+
+
+    public static ArrayList<HashMap<String, String>> queryByFatherID(int fatherID) {
+        return null;
+    }
+
+    public static HashMap<String, String> queryByID(int ID) {
+        return null;
+    }
+
+    public static int deleteSchedule(int scheduleID) {
+        return 0;
+    }
+
+    public static int updateStatus(int scheduleID, int status) {
+        return 0;
+    }
+
+}
