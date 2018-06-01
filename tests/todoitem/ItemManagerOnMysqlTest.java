4fb1b30c35a47ac7497bb7248ab81a0d50629272
Oops <16302010059@fudan.edu.cn>
2018-06-01 19:19:55 +0800

    ItemManager类的单元测试

--- tests/todoitem/ItemManagerOnMysqlTest.java
+++ tests/todoitem/ItemManagerOnMysqlTest.java
@@ -1,10 +1,10 @@
 package todoitem;
 
 import database.Mysql;
+import exception.DataErrorException;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import todoitem.itemSub.AppointmentItem;
-import todoitem.itemSub.MeetingItem;
-import todoitem.itemSub.OtherItem;
+import todoitem.itemSub.*;
 import todoitem.util.TimeStamp;
 
 import java.util.ArrayList;
@@ -12,6 +12,15 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class ItemManagerOnMysqlTest {
+    private static Mysql mysql;
+    private static ItemManager manager;
+
+    @BeforeClass
+    public static void init() {
+        mysql = Mysql.getInstance();
+        manager = ItemManager.getInstance();
+    }
+
     @Test
     public void getInstance() throws Exception {
         ItemManager manager1 = ItemManager.getInstance();
@@ -25,14 +34,12 @@ public class ItemManagerOnMysqlTest {
     @Test
     public void setCompleted() {
         try {
-            Mysql mysql = Mysql.getInstance();
             mysql.clear();
             // 只有处于进行中的待办事项才能设置成完成状态
 
             // 对于含有子待办事项的待办事项不能直接设置成完成状态
 
             // 当所有子待办事项都完成时，将父待办事项设置为完成状态
-            ItemManager manager = ItemManager.getInstance();
 
             ArrayList<Item> items = new ArrayList<Item>() {
                 {
@@ -69,7 +76,6 @@ public class ItemManagerOnMysqlTest {
                 manager.addItem(item, true);
 
             }
-//        ArrayList<Item> items = new ArrayList<>();
             items = manager.getItemsByStamp(
                     new TimeStamp(2018, 1, 1, 0, 0),
                     new TimeStamp(2022, 12, 31, 23, 59)
@@ -93,10 +99,6 @@ public class ItemManagerOnMysqlTest {
                     manager.addChildItem(item1);
                     manager.addChildItem(item2);
                     manager.setCompleted(item2);
-//                    ArrayList<Item> father = manager.getItemsByStamp(item.getFrom(), item.getTo());
-//                    for (Item it : father) {
-//                        assertEquals(Const.COMPLETED, it.getStatus());
-//                    }
                     item = manager.getItemByID(item.getID());
                     //TODO 获取新的父待办事项
                     assertEquals(Const.COMPLETED, item.getStatus());
@@ -111,21 +113,137 @@ public class ItemManagerOnMysqlTest {
 
     @Test
     public void updateStatus() throws Exception {
+        mysql.clear();
         // 测试能否正确更新状态
         // 1、未开始
 
         // 2、进行中
 
         // 3、过期
+        ArrayList<Item> items = new ArrayList<Item>() {
+            {
+                add(new OtherItem(new TimeStamp(2019, 5, 26, 0, 0),
+                        new TimeStamp(2019, 6, 1, 23, 50),
+                        "未开始"));
+                add(new AppointmentItem(new TimeStamp(2020, 2, 20, 0, 1),
+                        new TimeStamp(2021, 2, 26, 0, 2),
+                        "未开始", "", ""));
+
+                add(new TravelItem(new TimeStamp(2019, 5, 20, 5, 42),
+                        new TimeStamp(2021, 7, 30, 5, 43),
+                        "未开始", "", "", ""));
+
+                add(new OtherItem(new TimeStamp(2018, 5, 20, 0, 0),
+                        new TimeStamp(2019, 5, 24, 23, 50),
+                        "进行中"));
+
+                add(new AppointmentItem(new TimeStamp(2018, 4, 21, 23, 1),
+                        new TimeStamp(2019, 6, 30, 16, 6),
+                        "进行中", "", ""));
+
+                add(new TravelItem(new TimeStamp(2018, 5, 20, 5, 42),
+                        new TimeStamp(2022, 7, 30, 5, 43),
+                        "进行中", "", "", ""));
+
+                add(new OtherItem(new TimeStamp(2018, 5, 2, 0, 0),
+                        new TimeStamp(2018, 5, 2, 23, 59),
+                        "已过期"));
+
+                add(new AppointmentItem(new TimeStamp(2016, 5, 20, 5, 42),
+                        new TimeStamp(2017, 7, 30, 5, 43),
+                        "已过期", "", ""));
+
+                add(new TravelItem(new TimeStamp(2018, 5, 20, 5, 42),
+                        new TimeStamp(2018, 5, 30, 5, 43),
+                        "已过期", "", "", ""));
+            }
+
+        };
+        for (Item item : items) {
+            manager.updateStatus(item);// 根据当前时间更新状态
+        }
+
+        for (int i = 0; i < 3; i++) {
+            assertEquals(Const.BEFORE_BEGINNING, items.get(i).getStatus());//前三个更新后应为：未开始
+        }
+        for (int i = 3; i < 6; i++) {
+            assertEquals(Const.IN_PROGRESS, items.get(i).getStatus()); //第三至六个更新后为：进行中
+        }
+        for (int i = 6; i < items.size(); i++) {
+            assertEquals(Const.OVERDUE, items.get(i).getStatus());//剩下几个更新后为：已过期
+        }
     }
 
     @Test
     public void getItemsByStamp() throws Exception {
         // 测试能否正确查询到纪念日、课程信息
+        mysql.clear(); //保证可以添加下列待办事项（不出现时间重叠）
+        ArrayList<TimeStamp> startTimes = new ArrayList<TimeStamp>() {
+            {
+                add(new TimeStamp(2018, 5, 26, 0, 0));
+                add(new TimeStamp(2019, 2, 20, 0, 1));
+                add(new TimeStamp(2020, 5, 20, 5, 42));
+                add(new TimeStamp(2018, 5, 20, 0, 0));
+                add(new TimeStamp(2021, 4, 21, 23, 1));
+                add(new TimeStamp(2022, 1, 1, 0, 0));
+                add(new TimeStamp(2023, 5, 2, 0, 0));
+            }
+        };
+        ArrayList<TimeStamp> endTimes = new ArrayList<TimeStamp>() {
+            {
+                add(new TimeStamp(2018, 6, 1, 23, 50));
+                add(new TimeStamp(2019, 2, 26, 0, 2));
+                add(new TimeStamp(2020, 7, 30, 5, 43));
+                add(new TimeStamp(2018, 5, 20, 23, 50));
+                add(new TimeStamp(2021, 6, 30, 16, 6));
+                add(new TimeStamp(2022, 1, 1, 23, 59));
+                add(new TimeStamp(2023, 5, 2, 23, 59));
+            }
+        };
+        ArrayList<Item> items = new ArrayList<Item>() {
+            {
+                add(new OtherItem(startTimes.get(0), endTimes.get(0), ""));
+
+                add(new AppointmentItem(startTimes.get(1), endTimes.get(1), "", "", ""));
+
+                add(new TravelItem(startTimes.get(2), endTimes.get(2), "", "", "", ""));
+
+                add(new CourseItem(startTimes.get(3), endTimes.get(3), "", "", "", "", "", "", ""));
+
+                add(new MeetingItem(startTimes.get(4), endTimes.get(4), "", "", ""));
+
+                add(new AnniversaryItem(startTimes.get(5), endTimes.get(5), "", "", "新年"));
+
+                add(new InterviewItem(startTimes.get(6), endTimes.get(6), "", "", "", ""));
+
+            }
+
+        };
+        ArrayList<Item.ItemType> types = new ArrayList<Item.ItemType>() {
+            {
+                add(Item.ItemType.CUSTOM);
+                add(Item.ItemType.DATE);
+                add(Item.ItemType.TRAVEL);
+                add(Item.ItemType.COURSE);
+                add(Item.ItemType.MEETING);
+                add(Item.ItemType.ANNIVERSARY);
+                add(Item.ItemType.INTERVIEW);
+            }
+        };
+        for (Item item : items) {
+            manager.addItem(item, true);
+        }
+
+        for (int i = 0; i < startTimes.size(); i++) {
+            ArrayList<Item> resItems = manager.getItemsByStamp(startTimes.get(i), endTimes.get(i));
+            assertEquals(1, resItems.size());
+            assertEquals(types.get(i), resItems.get(0).getItemType());
+        }
     }
 
     @Test
     public void addItem() throws Exception {
+        mysql.clear();
         //一、测试互斥类型不允许作父子的情况
 
 
@@ -138,10 +256,192 @@ public class ItemManagerOnMysqlTest {
          * 2、课程
          */
 
+        ArrayList<TimeStamp> startTimes = new ArrayList<TimeStamp>() {
+            {
+                add(new TimeStamp(2018, 5, 26, 0, 0));
+                add(new TimeStamp(2018, 5, 26, 10, 0));
+                add(new TimeStamp(2018, 5, 25, 0, 1));
+                add(new TimeStamp(2018, 5, 26, 11, 0));
+
+                add(new TimeStamp(2020, 5, 20, 5, 42));
+                add(new TimeStamp(2020, 5, 20, 0, 0));
+                add(new TimeStamp(2020, 5, 18, 23, 1));
+                add(new TimeStamp(2020, 6, 1, 0, 0));
+            }
+        };
+        ArrayList<TimeStamp> endTimes = new ArrayList<TimeStamp>() {
+            {
+                add(new TimeStamp(2018, 6, 1, 23, 50));
+                add(new TimeStamp(2018, 5, 26, 12, 0));
+                add(new TimeStamp(2018, 5, 28, 0, 2));
+                add(new TimeStamp(2018, 5, 28, 0, 0));
+
+                add(new TimeStamp(2020, 7, 30, 5, 43));
+                add(new TimeStamp(2020, 5, 20, 23, 59));
+                add(new TimeStamp(2020, 5, 21, 16, 6));
+                add(new TimeStamp(2020, 8, 1, 23, 59));
+            }
+        };
+
+
+        //父待办事项
+        ArrayList<Item> items = new ArrayList<Item>() {
+            {
+                add(new AppointmentItem(startTimes.get(0), endTimes.get(0), "", "", ""));
+
+                add(new AppointmentItem(startTimes.get(4), endTimes.get(4), "", "", ""));
+            }
+        };
+
+        int fatherID1 = manager.addItem(items.get(0), true);
+        manager.addItem(items.get(1), true);
+
+        ArrayList<Item> children1 = new ArrayList<Item>() {
+            {
+                //添加为约会的子事项，
+                add(new OtherItem(startTimes.get(1), endTimes.get(1), ""));//成功
+                add(new AppointmentItem(startTimes.get(1), endTimes.get(1), "", "", ""));//成功
+                add(new InterviewItem(startTimes.get(1), endTimes.get(1), "", "", "", ""));//互斥，失败
+                add(new OtherItem(startTimes.get(2), endTimes.get(2), ""));//不在时间段内，失败
+                add(new AppointmentItem(startTimes.get(3), endTimes.get(3), "", "", ""));//时间重叠，失败
+            }
+        };
+        for (int i = 0; i < children1.size(); i++) {
+            children1.get(i).setFatherID(fatherID1);
+            children1.get(i).setIsFather(false);
+        }
+
+        boolean add = false;
+        try {
+            manager.addChildItem(children1.get(0));
+            add = true;
+        } catch (DataErrorException e) {
+
+        }
+        assertTrue(add);
+        add = false;
+        try {
+            manager.addChildItem(children1.get(1));
+            add = true;
+        } catch (DataErrorException e) {
+
+        }
+        assertTrue(add);
+        add = false;
+        try {
+            manager.addChildItem(children1.get(2));
+            add = true;
+        } catch (DataErrorException e) {
+
+        }
+        assertFalse(add);
+        add = false;
+        try {
+            manager.addChildItem(children1.get(3));
+            add = true;
+        } catch (DataErrorException e) {
+
+        }
+        assertFalse(add);
+        add = false;
+        try {
+            manager.addChildItem(children1.get(4));
+            add = true;
+        } catch (Exception e) {
+
+        }
+        assertFalse(add);
+        ArrayList<Item> children2 = new ArrayList<Item>() {
+            {
+                add(new AnniversaryItem(startTimes.get(5), endTimes.get(5), "", "", ""));//成功，纪念日允许重叠
+                add(new OtherItem(startTimes.get(5), endTimes.get(5), ""));//成功，自定义事项允许重叠
+                add(new InterviewItem(startTimes.get(5), endTimes.get(5), "", "", "", ""));//重叠，失败
+                add(new OtherItem(startTimes.get(6), endTimes.get(6), ""));//重叠，失败
+                add(new AppointmentItem(startTimes.get(7), endTimes.get(7), "", "", ""));//重叠，失败
+            }
+        };
+        add = false;
+        try {
+            manager.addItem(children2.get(0), true);
+            add = true;
+        } catch (Exception e) {
+
+        }
+        assertTrue(add);
+        add = false;
+        try {
+            manager.addItem(children2.get(1), true);
+            add = true;
+        } catch (Exception e) {
+
+        }
+        assertTrue(add);
+        add = false;
+        try {
+            manager.addItem(children2.get(2), true);
+            add = true;
+        } catch (Exception e) {
+
+        }
+        assertFalse(add);
+        add = false;
+        try {
+            manager.addItem(children2.get(3), true);
+            add = true;
+        } catch (Exception e) {
+
+        }
+        assertTrue(add);
+        add = false;
+        try {
+            manager.addItem(children2.get(4), true);
+            add = true;
+        } catch (Exception e) {
+
+        }
+        assertFalse(add);
+
+
     }
 
     @Test
     public void deleteItem() throws Exception {
+        mysql.clear();
+        ArrayList<Item> fathers = new ArrayList<Item>() {
+            {
+                add(new OtherItem(new TimeStamp(2018, 5, 20, 5, 42),
+                        new TimeStamp(2021, 7, 30, 5, 43), ""));
+
+                add(new OtherItem(new TimeStamp(2018, 5, 20, 5, 42),
+                        new TimeStamp(2022, 7, 30, 5, 43), ""));
+            }
+        };
+        assertTrue(manager.addItem(fathers.get(0), true) != 0);
+        assertTrue(manager.deleteItem(fathers.get(0)));
+
+        int id = manager.addItem(fathers.get(1), true);
+        assertTrue(id != 0);
+        ArrayList<Item> children = new ArrayList<Item>() {
+            {
+                add(new OtherItem(new TimeStamp(2018, 5, 20, 5, 42),
+                        new TimeStamp(2021, 7, 30, 5, 43), ""));
+                add(new OtherItem(new TimeStamp(2018, 5, 20, 5, 42),
+                        new TimeStamp(2021, 7, 30, 5, 43), ""));
+            }
+        };
+        for (Item aChildren : children) {
+            aChildren.setFatherID(id);
+            aChildren.setIsFather(false);
+            boolean add = false;
+            try {
+                manager.addChildItem(aChildren);
+                add = true;
+            } catch (Exception e) {
+
+            }
+            assertTrue(add);
+        }
+        assertTrue(manager.deleteItem(fathers.get(1)));
 
     }
 
